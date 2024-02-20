f3fad7687b0ab0648567c38b38afd33bdb42fb70
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-20 17:18:44 +0100

    add icon for parking

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include 'data.php'
-?> 
-
+?>
 
 
 <!DOCTYPE html>
@@ -10,17 +9,40 @@ include 'data.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--Font Awesome-->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous'/>
     <!--Bootstrap-->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous'/>
 </head>
-<body>
-    <ul>
-    <?php foreach ($hotels as  $hotel) : ?>
-            <?php foreach ($hotel as $info) : ?>
-                <li><?=$info?></li>
-            <?php endforeach; ?>
-    <?php endforeach; ?>
-    </ul>
- 
+<body class="container" >             
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Hotel Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as  $hotel) : ?>     
+                    <?php if($hotel['parking'] === true ) : ?> 
+                       <?php $hotel['parking'] = '<i class="fa-solid fa-circle-check"></i>'?>
+                    <?php else: ?>
+                    <?php $hotel['parking'] = '<i class="fa-solid fa-circle-xmark"></i>'
+                    ?>
+                    <?php endif; ?>
+                    <tr>     
+                        <td><?= $hotel['name'] ?></td>
+                        <td><?= $hotel['description'] ?></td>
+                        <td><?= $hotel['parking'] ?></td>
+                        <td><?= $hotel['vote'] ?></td>   
+                        <td><?= $hotel['distance_to_center'] ?></td>   
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
 </body>
 </html>
